4ae16e8828060bfaef2b7c27965900d0ad18589d
Andrew Kleindolph <akleindolph@gmail.com>
2020-09-25 09:20:07 -0700

    Update simple-neopixels.py

--- simple-neopixels.py
+++ simple-neopixels.py
@@ -7,7 +7,7 @@ pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.2, auto_write=False)
 
 BLUE = (0,0,255)
 RED = (255,0,0)
-GREEN = (255,0,0)
+GREEN = (0,255,0)
 
 while True:
     pixels.fill(BLUE)   #turn on all pixels
